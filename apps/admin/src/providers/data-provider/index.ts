69a7724f47c1f9a420b8a12e8533ffb429983dfd
Benito Pedro <benitopedro21@outlook.com>
2024-08-21 07:55:13 -0300

    👌 IMPROVE: new LOCAL_API_URL link

--- apps/admin/src/providers/data-provider/index.ts
+++ apps/admin/src/providers/data-provider/index.ts
@@ -3,7 +3,7 @@
 // const BACKEND_API_URL = "http://localhost:3000";
 // export const LOCAL_API_URL = "http://localhost:3001/api";
 const BACKEND_API_URL = "https://api.thatsbee.co";
-export const LOCAL_API_URL = "https://bee-dash-refine.vercel.app/api";
+export const LOCAL_API_URL = "https://admin.thatsbee.co/api";
 
 import type { DataProvider } from "@refinedev/core";
 
